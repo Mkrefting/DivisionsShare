23a9ec2d2f63db4cc2daee8878741bc1776998a9
Max <maxkrefting>
2021-05-23 13:18:29 +0100

    Initial Commit

---
+++ DivisionsShare/DivisionsShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  DivisionsShareApp.swift
+//  DivisionsShare
+//
+//  Created by Krefting, Max (PGW) on 23/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct DivisionsShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
